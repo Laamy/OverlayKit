0e4fada8e153617f4446c2b25a1a2e83c5c774a7
YeemiRouth <yeemirouth@gmail.com>
2024-04-21 15:19:01 +1000

    transparent winform

---
+++ TransparentWinForm/Form1.cs
@@ -0,0 +1,20 @@
+﻿using OverlayKit.Window;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TransparentWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            WinHandler.AddTransparencyLayer(Handle); // transparency key is 0,0,0,(0-255)
+
+            BackColor = Color.Black;
+
+            //TransparencyKey - windows forms also has this (though it excludes a few things like the actual layers)
+        }
+    }
+}
